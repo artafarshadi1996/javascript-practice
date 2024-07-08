d4415129b81619d01e83d31a9547f7ad75f136ac
arta <artafarshadi1996gmail.com>
2024-07-08 08:33:29 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// Step 1: Declare a new variable named petDog and give it the name Rex
+var petDog = "Rex";
+
+// Step 2: Declare a new variable named petCat and give it the name Pepper
+var petCat = "Pepper";
+
+// Step 3: Console.log the petDog variable
+console.log(petDog);
+
+// Step 4: Console.log the petCat variable
+console.log(petCat);
+
+// Step 5: Console.log the text "My pet dog's name is: " and the petDog variable
+console.log("My pet dog's name is: " + petDog);
+
+// Step 6: Console.log the text "My pet cat's name is: " and the petCat variable
+console.log("My pet cat's name is: " + petCat);
+
+// Step 7: Declare another variable and name it catSound. Assign the string "purr" to it
+var catSound = "purr";
+
+// Step 8: Declare another variable and name it dogSound. Assign the string "woof" to it
+var dogSound = "woof";
+
+// Step 9: Console.log the variable petDog, then the string "says", then the variable dogSound
+console.log(petDog + " says " + dogSound);
+
+// Step 10: Console.log the variable petCat, then the string "says", then the variable catSound
+console.log(petCat + " says " + catSound);
+
+// Step 11: Reassign the value stored in catSound to the string "meow"
+catSound = "meow";
+
+// Step 12: Console.log the variable petCat, then the string "now says", then the variable catSound
+console.log(petCat + " now says " + catSound);
